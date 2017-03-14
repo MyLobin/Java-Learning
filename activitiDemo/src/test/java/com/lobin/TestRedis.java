1c8971bbfb5183fca2a7c9103d4f3815e195557e
mylobin <Lobin@localhost>
2017-03-14 16:10:39 +0800

    add redis

---
+++ activitiDemo/src/test/java/com/lobin/TestRedis.java
@@ -0,0 +1,25 @@
+package com.lobin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import junit.framework.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@SpringBootTest(classes=Application.class) 
+@WebAppConfiguration
+public class TestRedis {
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	@Test
+	public void test() throws Exception {
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+}
